3d222046a0700a5e7061953f60ad62a712b22a0c
Bynow(伯言) <1923184056@qq.com>
2020-06-14 18:03:07 +0800

    build

---
+++ tempest-web/src/main/java/cn/bynow/tempest/web/TempestWebApplication.java
@@ -0,0 +1,13 @@
+package cn.bynow.tempest.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TempestWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TempestWebApplication.class, args);
+    }
+
+}
---
+++ tempest-web/src/test/java/cn/bynow/tempest/web/TempestWebApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.bynow.tempest.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TempestWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
