3f0e25eee50afa2aca50bd31ffe3a2f921862ebd
Bynow(伯言) <1923184056@qq.com>
2020-06-16 01:25:33 +0800

    引入反向工程asuka

---
+++ tempest-asuka/src/main/java/cn/bynow/tempest/asuka/entity/MysqlDominEntity.java
@@ -0,0 +1,22 @@
+package cn.bynow.tempest.asuka.entity;
+
+
+import lombok.Data;
+
+/**
+ * @author Bynow
+ * @version 1.0
+ * @date 2020/6/11 11:33
+ */
+@Data
+public class MysqlDominEntity {
+    private String filePath;
+    private String author;
+    private String packagePath;
+    private String password;
+    private String tableName;
+    private String database;
+    private String user;
+
+
+}
